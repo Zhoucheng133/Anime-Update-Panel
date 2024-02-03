6961cf78067987926e96b0d9de4a47672621f1a6
zhoucheng <zhoucheng133@outlook.com>
2024-02-03 17:29:05 +0800

    显示当前的weekday

--- lib/Views/components/weekdayInfo.dart
+++ lib/Views/components/weekdayInfo.dart
@@ -1,6 +1,5 @@
-// ignore_for_file: file_names, camel_case_types, prefer_const_constructors
-
-import 'package:flutter/material.dart';
+// ignore_for_file: file_names, camel_case_types, prefer_const_constructors, prefer_const_literals_to_create_immutables
+import 'package:fluent_ui/fluent_ui.dart';
 
 class weekdayInfo extends StatefulWidget {
   final String day;
@@ -12,6 +11,36 @@ class weekdayInfo extends StatefulWidget {
 }
 
 class _weekdayInfoState extends State<weekdayInfo> {
+
+  String getDayOfWeek(int day) {
+    switch (day) {
+      case 1:
+        return '星期一';
+      case 2:
+        return '星期二';
+      case 3:
+        return '星期三';
+      case 4:
+        return '星期四';
+      case 5:
+        return '星期五';
+      case 6:
+        return '星期六';
+      case 7:
+        return '星期日';
+      default:
+        return '';
+    }
+  }
+
+  bool checkToday(){
+    DateTime now = DateTime.now();
+    if(widget.day==getDayOfWeek(now.weekday)){
+      return true;
+    }
+    return false;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -20,12 +49,27 @@ class _weekdayInfoState extends State<weekdayInfo> {
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text(
-            widget.day,
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 20
-            ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                widget.day,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20
+                ),
+              ),
+              SizedBox(width: 15,),
+              checkToday()==true ? Container(
+                height: 15,
+                width: 15,
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 0, 204, 144),
+                  borderRadius: BorderRadius.circular(15),
+                ),
+              ) : Container()
+            ],
           )
         ],
       ),
