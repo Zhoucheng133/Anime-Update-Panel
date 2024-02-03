5769c767dd423355bade46fda3ce30c12da109bf
zhoucheng <zhoucheng133@outlook.com>
2024-02-03 17:46:26 +0800

    修改控制面板

--- lib/Views/components/controlPanel.dart
+++ lib/Views/components/controlPanel.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: camel_case_types, file_names, prefer_const_constructors
+// ignore_for_file: camel_case_types, file_names, prefer_const_constructors, prefer_const_literals_to_create_immutables, sort_child_properties_last
 
 import 'package:fluent_ui/fluent_ui.dart';
 // import 'package:flutter/material.dart';
@@ -11,76 +11,20 @@ class controlPanel extends StatefulWidget {
 }
 
 class _controlPanelState extends State<controlPanel> {
-
-    Future<void> addAnime(BuildContext context) async {
-    
-    TextEditingController name=TextEditingController();
-    DateTime? updateDate;
-
-    
-    final result = await showDialog<String>(
-      context: context,
-      builder: (context) => ContentDialog(
-        title: Text('添加一个番剧'),
-        content: StatefulBuilder(
-          builder: (BuildContext context, StateSetter setState){
-            return SizedBox(
-              height: 250,
-              width: 300,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "名称",
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 18
-                    ),
-                  ),
-                  SizedBox(height: 10,),
-                  TextBox(
-                    controller: name,
-                  ),
-                  SizedBox(height: 30,),
-                  Text(
-                    "更新时间",
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 18
-                    ),
-                  ),
-                  SizedBox(height: 10,),
-                  DatePicker(
-                    selected: updateDate,
-                    onChanged: (time) => setState(() => updateDate = time),
-                    showYear: false,
-                  )
-                ],
-              ),
-            );
-          }
-        ),
-        actions: [
-          Button(
-            child: const Text('取消'),
-            onPressed: () {
-              Navigator.pop(context, 'User deleted file');
-              // Delete file here
-            },
-          ),
-          FilledButton(
-            child: const Text('完成'),
-            onPressed: () => Navigator.pop(context, 'User canceled dialog'),
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Icon(
+            FluentIcons.app_icon_default_edit,
+            size: 20,
           ),
         ],
       ),
     );
-  }
-
-
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
   }
 }
